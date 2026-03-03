dc0b65c6d06d8629f0a6a1f582bcda17f8ba6ff4
Bruce Brown <brown.bruce32@gmail.com>
2026-03-03 14:39:11 -0600

    Added Reservation controller

---
+++ reservation-service/reservation-service/src/main/java/com/bbrown/reservation/controller/ReservationController.java
@@ -0,0 +1,34 @@
+package com.bbrown.reservation.controller;
+
+import com.bbrown.reservation.dto.ReservationRequest;
+import com.bbrown.reservation.dto.ReservationResponse;
+import com.bbrown.reservation.mapper.ReservationMapper;
+import com.bbrown.reservation.model.Reservation;
+import com.bbrown.reservation.service.ReservationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService service;
+    private final ReservationMapper mapper;
+
+    @PostMapping
+    public ReservationResponse create(@RequestBody @Valid ReservationRequest request) {
+        Reservation reservation = mapper.toEntity(request);
+        Reservation saved = service.create(reservation);
+        return mapper.toResponse(saved);
+    }
+
+    @GetMapping("/{id}")
+    public ReservationResponse get(@PathVariable UUID id) {
+        return mapper.toResponse(service.get(id));
+    }
+
+}
