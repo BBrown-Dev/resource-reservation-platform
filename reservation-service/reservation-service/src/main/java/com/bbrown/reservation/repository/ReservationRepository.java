4acd37975c1d8e177df4eb96bf2e19f6d21ff771
Bruce Brown <brown.bruce32@gmail.com>
2026-03-03 14:39:47 -0600

    Added Reservation repository

---
+++ reservation-service/reservation-service/src/main/java/com/bbrown/reservation/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.bbrown.reservation.repository;
+
+import com.bbrown.reservation.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+}
